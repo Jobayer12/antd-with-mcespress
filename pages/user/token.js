5261b444b8004a5d3789fbaa5bb8a07f91c0993c
ONIK <jobayer.cse43.diu@gmail.com>
2020-03-21 11:02:49 +0600

    token verify

--- pages/user/token.js
+++ pages/user/token.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../../less/token.less";
-import { Result, Button } from "antd";
+import { Result, Button, Spin } from "antd";
 import queryString from "query-string";
 import { withRouter } from "next/router";
 import Layout from "../../components/Layout";
@@ -11,6 +11,7 @@ class Token extends Component {
     super(props);
     this.state = {
       isLoading: false,
+      isSuccess: true,
       search: queryString.parse(this.props.router.asPath.split(/\?/)[1]),
       error: "",
       response: ""
@@ -20,15 +21,18 @@ class Token extends Component {
     this.setState({ isLoading: true });
     userVerify(this.state.search.token)
       .then(result => {
-        console.log("result", result);
-        this.setState({ response: result, isLoading: false });
+        this.setState({
+          response: result,
+          isLoading: false,
+          isSuccess: result.message.success
+        });
       })
       .catch(this.showError);
   }
   showError = err => {
     const error = (err.response && err.response.data) || err.message;
     console.log(error);
-    this.setState({ error: error.errors, isLoading: false });
+    this.setState({ error: error.errors, isLoading: false, isSuccess: false });
   };
 
   render() {
@@ -36,31 +40,35 @@ class Token extends Component {
 
     return (
       <Layout>
-        <div className="success">
-          {isLoading ? (
-            <Result
-              status="success"
-              title="Account verified Successfully."
-              subTitle="Now you can login"
-              extra={[
-                <Button href="/user/signin" type="primary" key="console">
-                  Go Login page
-                </Button>
-              ]}
-            />
-          ) : (
-            <Result
-              status="warning"
-              title="Invalid token"
-              subTitle="your token are invalid.Go to verification page to get another token"
-              extra={
-                <Button href="/user/verify" type="primary" key="console">
-                  Go Verify page
-                </Button>
-              }
-            />
-          )}
-        </div>
+        {isLoading ? (
+          <Spin className="example" />
+        ) : (
+          <div className="success">
+            {isSuccess ? (
+              <Result
+                status="success"
+                title="Account verified Successfully."
+                subTitle="Now you can login"
+                extra={[
+                  <Button href="/user/signin" type="primary" key="console">
+                    Go Login page
+                  </Button>
+                ]}
+              />
+            ) : (
+              <Result
+                status="warning"
+                title="Invalid token"
+                subTitle="your token are invalid.Go to verification page to get another token"
+                extra={
+                  <Button href="/user/verify" type="primary" key="console">
+                    Go Verify page
+                  </Button>
+                }
+              />
+            )}
+          </div>
+        )}
       </Layout>
     );
   }
